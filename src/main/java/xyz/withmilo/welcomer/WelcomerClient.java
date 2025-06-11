001d25f582abf2e30261d138fe422df324dd8f36
chasemarshall <chasemarshall.f@gmail.com>
2025-06-10 22:21:42 -0500

    auto send hi on seeing [+] anywhere in chat

--- src/main/java/xyz/withmilo/welcomer/WelcomerClient.java
+++ src/main/java/xyz/withmilo/welcomer/WelcomerClient.java
@@ -1,10 +1,21 @@
 package xyz.withmilo.welcomer;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
+import net.minecraft.client.MinecraftClient;
 
 public class WelcomerClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        ClientReceiveMessageEvents.CHAT.register((message, signedMessage, sender, params, timestamp) -> {
+            String msg = message.getString();
+            System.out.println("DEBUG: Received chat message: [" + msg + "]");
+            if (msg.contains("[+]")) { // More flexible matching
+                MinecraftClient client = MinecraftClient.getInstance();
+                if (client.player != null && client.player.networkHandler != null) {
+                    client.player.networkHandler.sendChatMessage("hi");
+                }
+            }
+        });
     }
 }
