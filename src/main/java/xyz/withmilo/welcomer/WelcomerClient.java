f7a1b3444163e157e6757e4e3e5f97e5c462dfca
chasemarshall <chasemarshall.f@gmail.com>
2025-06-10 21:34:05 -0500

    Initial commit

---
+++ src/main/java/xyz/withmilo/welcomer/WelcomerClient.java
@@ -0,0 +1,10 @@
+package xyz.withmilo.welcomer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WelcomerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
